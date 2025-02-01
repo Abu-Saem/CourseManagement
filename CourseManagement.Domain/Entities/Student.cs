7dd6c333503635b190798d7349ebd81d2f3d5d30
Abu-Saem <abusaem1774@gmail.com>
2025-02-01 23:45:54 +0600

    Student domain added

---
+++ CourseManagement.Domain/Entities/Student.cs
@@ -0,0 +1,16 @@
+﻿using CourseManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagement.Domain.Entities
+{
+    public class Student : BaseEntity
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public ICollection<Course> Courses { get; set; } = new List<Course>();
+    }
+}
