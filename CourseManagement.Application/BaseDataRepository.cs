1c8dd91fdab87359c714cc5e63c8336b9174b050
Abu-Saem <abusaem1774@gmail.com>
2025-02-02 23:29:39 +0600

    base data manager & interface for dept added

---
+++ CourseManagement.Application/BaseDataRepository.cs
@@ -0,0 +1,92 @@
+﻿using CourseManagement.Infrastructure.DbModel;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+
+namespace CourseManagement.Application
+{
+    public abstract class BaseDataRepository
+    {
+        protected CourseManagementDbContext dbModel;
+        public BaseDataRepository(CourseManagementDbContext model)
+        {
+            dbModel = model;
+        }
+
+        protected T FindEntity<T>(int primaryKey) where T : class
+        {
+            try
+            {
+                return dbModel.Set<T>().Find(primaryKey);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+                throw;
+            }
+        }
+
+        protected void AddUpdateEntity<T>(T entity, bool keepDettached = true) where T : class
+        {
+            try
+            {
+                if (dbModel.Entry<T>(entity).IsKeySet)
+                    dbModel.Update<T>(entity);
+                else
+                    dbModel.Add<T>(entity);
+
+                dbModel.SaveChanges();
+
+                if (keepDettached)
+                {
+                    dbModel.Entry<T>(entity).State = EntityState.Detached;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+                throw;
+            }
+        }
+
+        protected void RemoveEntity<T>(int id) where T : class
+        {
+            try
+            {
+                var entity = dbModel.Set<T>().Find(id);
+                dbModel.Remove<T>(entity);
+                dbModel.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+                throw;
+            }
+        }
+        protected IList<T> GetListData<T>() where T : class
+        {
+            try
+            {
+                return dbModel.Set<T>().ToList();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+                throw;
+            }
+        }
+        protected IList<T> GetListData<T>(string interpolatedStoredProc, params object[] parameters) where T : class
+        {
+            try
+            {
+                return dbModel.Set<T>().FromSqlRaw(interpolatedStoredProc, parameters).AsQueryable().ToList();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+                throw;
+            }
+        }
+    }
+}
